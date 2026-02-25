dfd66f1567ddb89989a1294048d24e7b90f95987
Jan Chrobak <jan.chrobak02@outlook.com>
2026-02-25 19:45:40 +0100

    adjust notification styling

--- src/components/ui/Notifications.tsx
+++ src/components/ui/Notifications.tsx
@@ -6,7 +6,7 @@ export default function Notifications() {
 
     return <>
         {
-            notifications.length > 0 && <ul className='z-1000 fixed right-1/2 translate-x-1/2 bottom-4 max-w-[calc(100%-2rem)] lg:right-4 lg:bottom-4 lg:translate-x-0 flex flex-col gap-y-2'>
+            notifications.length > 0 && <ul className='z-1000 fixed right-1/2 translate-x-1/2 bottom-0 max-w-[calc(100%-2rem)] lg:right-4 lg:translate-x-0 flex flex-col gap-y-2'>
                 {
                     notifications.map((notification) => (
                         <li className="animate-entrance text-[#f7f7f7] p-3 rounded-md w-75 min-h-7 text-sm bg-emerald-500" key={notification.id}>{notification.message}</li>

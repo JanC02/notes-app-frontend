8cb325fa5350d12f3ff67425430bf25307117c4e
Jan Chrobak <jan.chrobak02@outlook.com>
2026-03-03 19:57:22 +0100

    add general 404 error page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <div className="h-screen flex justify-center items-center bg-linear-to-br from-stone-100 via-stone-200 to-stone-300">
+            <p className="text-4xl font-bold text-[#404040]">404 Not Found</p>
+        </div>
+    );
+};
+
+export default NotFound;

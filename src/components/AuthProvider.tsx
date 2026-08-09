a29a1d0a42559f6d7e3867a17d470cffe0e125af
Jan Chrobak <jan.chrobak02@outlook.com>
2026-08-09 08:26:59 +0200

    add spinner during session verification

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -1,13 +1,16 @@
 import { type ReactNode, useEffect } from "react";
 import { verifySession } from "../store/slices/auth";
 import { useDispatch } from "react-redux";
-import { type AppDispatch } from "../store/store";
+import type { AppDispatch, RootState } from "../store/store";
+import { useSelector } from "react-redux";
+import Spinner from "./ui/Spinner";
 
 interface AuthProviderProps {
     children: ReactNode
 };
 
 export default function AuthProviderProps({ children }: AuthProviderProps) {
+    const isVerificationPending = useSelector((state: RootState) => state.auth.isSessionVerificationPending);
     const dispatch = useDispatch<AppDispatch>();
 
     useEffect(() => {
@@ -15,6 +18,11 @@ export default function AuthProviderProps({ children }: AuthProviderProps) {
     }, [dispatch]);
 
     return <>
-        {children}
+        {
+            isVerificationPending ?
+                <div className="h-screen flex justify-center items-center">
+                    <Spinner className='text-[#404040] w-13 h-13' />
+                </div> : children
+        }
     </>
 }
